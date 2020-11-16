532dca24c474a2fb03b21469fd1eb6158f6439a8
Marike Elwers <m.e.elwers@gmail.com>
2020-11-16 17:27:09 +0100

    added index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "loading seed"
+ArtPiece.create!(title: 'Golden Vase', description: 'Old Vase with golden details.', price: '100', availability: true, user: User.first)
+ArtPiece.create!(title: 'Black Vase', description: 'Old Vase with black details.', price: '150', availability: true, user: User.first)
+ArtPiece.create!(title: 'Green Vase', description: 'Old Vase with green details.', price: '150', availability: true, user: User.first)
+puts "created seed"
