4f52e0198495ef71ba896a5e9e98e8bbbddf00d4
Lorenz Cronenberg <lorenz.cronenberg@gmx.de>
2020-11-16 18:11:47 +0100

    added new and create actions plus the associated views

--- app/controllers/art_pieces_controller.rb
+++ app/controllers/art_pieces_controller.rb
@@ -1,2 +1,20 @@
 class ArtPiecesController < ApplicationController
+  def new
+    @art_piece = ArtPiece.new
+  end
+
+  def create
+    @art_piece = ArtPiece.new(art_piece_params)
+    if @art_piece.save
+      redirect_to art_piece_path(@art_piece) # redirects to the show page
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def art_piece_params # params for security reasons
+    params.require(:art_piece).permit(:title, :description, :price) # add photos: [] if user can upload a photo too
+  end
 end
--- app/models/art_piece.rb
+++ app/models/art_piece.rb
@@ -1,3 +1,7 @@
 class ArtPiece < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { only_integer: true }
 end
