9c26e38656f2d3a053826428ac298b70804e7e50
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2020-11-16 16:17:36 +0100

    art pieces controller and roouting done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :art_pieces do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:destroy, :show, :update]
 end
