c3b48903438d00502665e752a3744a1bd90268fe
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2020-11-18 23:07:20 +0100

    rolled back the js script used, too hard

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -48,16 +48,3 @@ document.addEventListener('turbolinks:load', () => {
     })
   };
 });
-
-;(function(){
-  $(document).ready(function() {
-      $( '.dropdown' ).hover(
-          function(){
-              $(this).children('.submenu').slideDown(200);
-          },
-          function(){
-              $(this).children('.submenu').slideUp(200);
-          }
-      );
-  });
-})();
