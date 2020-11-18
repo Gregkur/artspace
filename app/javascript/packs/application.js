c0693d495903d2978a7dfaaf7ea4f4eb7c1746e8
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2020-11-18 23:06:39 +0100

    changed the navbar to show more and name instead of email

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -48,3 +48,16 @@ document.addEventListener('turbolinks:load', () => {
     })
   };
 });
+
+;(function(){
+  $(document).ready(function() {
+      $( '.dropdown' ).hover(
+          function(){
+              $(this).children('.submenu').slideDown(200);
+          },
+          function(){
+              $(this).children('.submenu').slideUp(200);
+          }
+      );
+  });
+})();
