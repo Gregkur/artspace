c337dea95a9b49ff8943e86c0161756ca18c08e3
Grzegorz Kurzajczyk <grzegorz.kurzajczyk@gmail.com>
2020-11-18 14:55:04 +0100

    changes migrations/author to artist

--- app/models/art_piece.rb
+++ app/models/art_piece.rb
@@ -6,5 +6,5 @@ class ArtPiece < ApplicationRecord
   validates :description, presence: true, length: { minimum: 75, too_short: "minimum is %{count} characters" }
   validates :price, presence: true, numericality: { only_integer: true }
   validates :photos, presence: true
-  validates :artist, presence: true, length: { minimum: 5 }
+  validates :artist, presence: true
 end
