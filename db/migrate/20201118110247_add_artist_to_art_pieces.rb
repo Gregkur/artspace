65ab1141abbe16cb0102deb04de1773b0ad1f2a2
Lorenz Cronenberg <lorenz.cronenberg@gmx.de>
2020-11-18 12:13:35 +0100

    created new migration file to add artist, added artist to create form and added some extra validations and messages

--- db/migrate/20201118110247_add_artist_to_art_pieces.rb
+++ db/migrate/20201118110247_add_artist_to_art_pieces.rb
@@ -1,5 +1,5 @@
 class AddArtistToArtPieces < ActiveRecord::Migration[6.0]
   def change
-    add_column :art_pieces, :author, :string
+    add_column :art_pieces, :artist, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_100041) do
+ActiveRecord::Schema.define(version: 2020_11_18_110247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 2020_11_17_100041) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "author"
+    t.string "full_name"
+    t.string "artist"
     t.index ["user_id"], name: "index_art_pieces_on_user_id"
   end
 
@@ -67,6 +69,8 @@ ActiveRecord::Schema.define(version: 2020_11_17_100041) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
